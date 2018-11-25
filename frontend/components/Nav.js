8250079fb72aced82a1cb1ffe4a32d8d348f0d17
Rubicker <tianxing@liepin.com>
2018-11-25 12:01:23 +0800

    :heavy_plus_sign: Add styled-component & deal css ssr render

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,11 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <div>
-    <Link href="/">
-      <a>Home!</a>
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
     </Link>
-  </div>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
