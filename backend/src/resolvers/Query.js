2aaf20bd14e79ceec8f0a726d3305bf1464c2e1d
Rubicker <newsl00001@gmail.com>
2018-12-03 22:57:50 +0800

    :heavy_plus_sign: {Backend} Add graphql-yoga and prisma

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding'); // 后端无需业务逻辑的查询，可以直接查询 prisma
+
+const Query = {
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
