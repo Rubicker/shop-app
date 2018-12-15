5e1fcd869660371c8b8c8b8cd1d205d462e3193f
Rubicker <newsl00001@gmail.com>
2018-12-15 10:37:24 +0800

    :sparkles: {Frontend & Backend} EditPage / Add updateItem

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require('prisma-binding'); // 后端无需业务逻辑的查询，可以直接查询 prisma
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
